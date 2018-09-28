a81285979479d6d2847521f4892a459d5497f59a
pochi-sato <foreignduck.domesticduck@gmail.com>
2018-09-28 11:41:39 +0900

    add type

--- getRandomString.js
+++ getRandomString.js
@@ -1,12 +1,21 @@
 const argLength = parseInt(process.argv[2]);
+const argType = process.argv[3];
 let length = 10;
+let charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 if(argLength > 0) {
   length = argLength;
 }
 
+if(argType === 'lower') {
+  charSet = 'abcdefghijklmnopqrstuvwxyz0123456789';
+} else if(argType === 'upper') {
+  charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+} else if(argType === 'number') {
+  charSet = '0123456789';
+}
+
 // 生成する文字列に含める文字セット
-const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 const charSetNum = charSet.length;
 let result = '';
 for(let i = 0; i < length; i++){
